e09ad3ce209e951c48e25f03cbd54af8045de17f
Luis Felipe <luisfelipekai@gmail.com>
2020-08-26 16:03:31 -0300

    Commit para o artigo.

---
+++ ArtigoIMemoryCache.API/Controllers/CountriesController.cs
@@ -0,0 +1,56 @@
+﻿using ArtigoIMemoryCache.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ArtigoIMemoryCache.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        private const string COUNTRIES_KEY = "Countries";
+
+        public CountriesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+
+            if (_memoryCache.TryGetValue(COUNTRIES_KEY, out object countriesObject))
+            {
+                return Ok(countriesObject);
+            } else
+            {
+                const string restCountriesUrl = "https://restcountries.eu/rest/v2/all";
+
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(restCountriesUrl);
+
+                    var responseData = await response.Content.ReadAsStringAsync();
+
+                    var countries = JsonConvert.DeserializeObject<List<Country>>(responseData);
+
+                    var memoryCacheEntryOptions = new MemoryCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                        SlidingExpiration = TimeSpan.FromSeconds(1200)
+                    };
+
+                    _memoryCache.Set(COUNTRIES_KEY, countries, memoryCacheEntryOptions);
+
+                    return Ok(countries);
+                }
+            }
+            
+        }
+    }
+}
---
+++ ArtigoIMemoryCache.API/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtigoIMemoryCache.API.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Region { get; set; }
+        public string Denonym { get; set; }
+    }
+}
